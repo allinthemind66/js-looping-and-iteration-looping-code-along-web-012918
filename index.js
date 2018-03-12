ffa2745477c505a99d9aa729af0583bb6990e18e
allinthemind66 <r.nilon92@gmail.com>
2018-03-11 20:09:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function printBadges(arr){
+  for(let i = 0; i < arr.length; i++){
+    console.log(`Welcome ${arr[i]}! You are employee #${i+1}.`)
+  };
+  return arr
+};
+
+
+function coinFlip(){
+  return Math.random() >= .5
+}
+function tailsNeverFails(){
+  let numTimes = 0
+  while (coinFlip() !== false){
+    numTimes++
+
+  }
+  return `You got ${numTimes} tails in a row!`
+};
